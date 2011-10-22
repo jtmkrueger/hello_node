48ae03f10a2fe18c016e81443185118c7ee2da61
jtmkrueger <jtmkrueger@gmail.com>
2011-10-22 04:28:32 +0000

    update

--- hello_node.js
+++ hello_node.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello Cloud9 IDE! This server is running live');
 }).listen(process.env.C9_PORT, "0.0.0.0");
 console.log('Server running at http://127.0.0.1:1337/');
