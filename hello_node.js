bfc8f1baeddab4dc9f5c5c668e5384dc4905d3fb
jtmkrueger <jtmkrueger@gmail.com>
2011-10-22 04:40:07 +0000

    change port

--- hello_node.js
+++ hello_node.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Cloud9 IDE! This server is running live');
-}).listen(process.env.C9_PORT, "0.0.0.0");
+}).listen(process.env.PORT, "0.0.0.0");
 console.log('Server running at http://127.0.0.1:1337/');
