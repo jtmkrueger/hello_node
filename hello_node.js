6fb48e0afcca35df6bd4f2b2731164d70e196c9c
jtmkrueger <jtmkrueger@gmail.com>
2011-10-22 04:41:28 +0000

    file change

--- hello_node.js
+++ hello_node.js
@@ -1,6 +1,9 @@
+var PORT = process.env.PORT;
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Cloud9 IDE! This server is running live');
-}).listen(process.env.PORT, "0.0.0.0");
-console.log('Server running at http://127.0.0.1:1337/');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {"Content-Type" : "text/plain"});
+    res.end("hello Node.js");
+});
+
+server.listen(PORT);
